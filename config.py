b26851c9f0df21783ffaded04d4bad2f5e100db4
jhil-chatterjee03 <137144167+jhil-chatterjee03@users.noreply.github.com>
2025-07-30 13:24:18 +0530

    Removed config.py from Git tracking

--- config.py
+++
@@ -1,9 +0,0 @@
-import praw
-
-reddit = praw.Reddit(
-    client_id='GzWK9z9QVeDNMYiQvHS8pw',
-    client_secret='HOi2QR0xzK6p0hkLIWDdEUZvFmNLOQ',
-    user_agent='Sentiment Analysis',
-    username='Icy_Connection_6109',
-    password='jhil123#456'
-)
